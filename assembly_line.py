b391d3970f98178c0fbbcbd37fbf562355aedfc5
Becka <rebecca.lelew@gmail.com>
2019-01-12 17:15:08 +0000

    Adds new components to belt and takes away last component

--- assembly_line.py
+++ assembly_line.py
@@ -1,4 +1,17 @@
+import random
 class Assembly_Line:
 
     def __init__(self, length = 3):
         self.belt = [None] * length
+
+    def new_component(self, randint = None):
+        components = {1: 'A', 2: 'B', 3: None}
+        if randint is None:
+            random_n = random.randint(1, 3)
+        else:
+            random_n = randint()
+        return components[random_n]
+
+    def progress_time(self, randint = None):
+        self.belt.insert(0, self.new_component(randint))
+        del self.belt[-1]
--- test_assembly_line.py
+++ test_assembly_line.py
@@ -1,9 +1,26 @@
+import random
+import mock
 from assembly_line import Assembly_Line
 
+def patched_randints():
+    return 1
+
+al = Assembly_Line()
+
 def test_assembly_line_initializes_with_default_size_belt():
-    al = Assembly_Line()
     assert len(al.belt) == 3
 
 def test_assembly_line_initializes_with_empty_belt():
-    al = Assembly_Line()
     assert all([slot==None for slot in al.belt])
+
+def test_item_generator_generates_components():
+    al = Assembly_Line()
+    assert al.new_component(randint = patched_randints) == 'A'
+
+def test_time_unit_produces_new_component_in_slot_1():
+    al.progress_time(randint = patched_randints)
+    assert al.belt[0] == 'A'
+
+def test_time_unit_keeps_belt_length_the_same():
+    al.progress_time()
+    assert len(al.belt) == 3
