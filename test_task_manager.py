87c4520ffe371508cf47c208e03f112869c06ae3
Becka <rebecca.lelew@gmail.com>
2019-01-12 21:21:27 +0000

    refactors task manager tests

--- test_task_manager.py
+++ test_task_manager.py
@@ -2,6 +2,9 @@ from task_manager import Task_Manager
 from doubles import ObjectDouble, allow
 from assembly_line import Assembly_Line
 from worker import Worker
+import pytest
+
+#Setup
 
 def randint_comp_A():
     return 1
@@ -12,39 +15,45 @@ def randint_comp_B():
 def randint_comp_none():
     return 3
 
-def test_task_manager_allocates_workers_to_assembly_line():
-    tm = Task_Manager()
-    assert len(tm.workers) == 3
-    assert all([len(pair) == 2 for pair in tm.workers])
+def generate_two_components(tm, identical = False):
+    tm.allocate_resources(randint_comp_A)
+    if identical:
+        tm.allocate_resources(randint_comp_A)
+    else:
+        tm.allocate_resources(randint_comp_B)
 
-def test_task_manager_allocates_component_A_to_worker_a1():
-    tm = Task_Manager()
-    tm.allocate_resources(randint_comp_A)
-    assert 'A' in tm.workers[0][0].components
-    assert 'A' not in tm.assembly_line.belt
+@pytest.fixture(autouse=True)
+def new_tm():
+    return Task_Manager()
 
-def test_task_manager_allocates_component_B_to_worker_a1():
-    tm = Task_Manager()
-    tm.allocate_resources(randint_comp_A)
-    tm.allocate_resources(randint_comp_B)
-    assert 'A' in tm.workers[0][0].components
-    assert 'B' in tm.workers[0][0].components
-    assert not tm.workers[0][0].collecting_mode
+#Tests
 
-def test_task_manager_does_not_allocate_identical_components_to_worker():
-    tm = Task_Manager()
-    tm.allocate_resources(randint_comp_A)
-    tm.allocate_resources(randint_comp_A)
-    assert tm.workers[0][0].components.count('A') == 1
-    assert tm.workers[0][1].components.count('A') == 1
+def test_task_manager_allocates_workers_to_assembly_line(new_tm):
+    assert len(new_tm.workers) == 3
+    assert all([len(pair) == 2 for pair in new_tm.workers])
 
-def test_task_manager_instructs_worker_to_place_widget_if_slot_empty():
-    tm = Task_Manager()
-    tm.allocate_resources(randint_comp_A)
-    tm.allocate_resources(randint_comp_B)
-    tm.allocate_resources(randint_comp_none)
-    tm.allocate_resources(randint_comp_none)
-    tm.allocate_resources(randint_comp_none)
-    tm.allocate_resources(randint_comp_none)
-    tm.allocate_resources(randint_comp_none)
-    assert 'X' in tm.assembly_line.belt
+def test_task_manager_allocates_component_A_to_worker_a1(new_tm):
+    new_tm.allocate_resources(randint_comp_A)
+    first_worker = new_tm.workers[0][0]
+    assert 'A' in first_worker.components
+    assert 'A' not in new_tm.assembly_line.belt
+
+def test_task_manager_allocates_component_B_to_worker_a1(new_tm):
+    generate_two_components(new_tm, identical = False)
+    first_worker = new_tm.workers[0][0]
+    assert 'A' in first_worker.components
+    assert 'B' in first_worker.components
+    assert not first_worker.collecting_mode
+
+def test_task_manager_does_not_allocate_identical_components_to_worker(new_tm):
+    generate_two_components(new_tm, identical = True)
+    first_worker = new_tm.workers[0][0]
+    second_worker = new_tm.workers[0][1]
+    assert first_worker.components.count('A') == 1
+    assert second_worker.components.count('A') == 1
+
+def test_task_manager_instructs_worker_to_place_widget_if_slot_empty(new_tm):
+    generate_two_components(new_tm, identical = False)
+    for i in range(5):
+        new_tm.allocate_resources(randint_comp_none)
+    assert 'X' in new_tm.assembly_line.belt
