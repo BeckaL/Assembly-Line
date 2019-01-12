9b284397235c61180629d6723f51717e0fdb53f8
Becka <rebecca.lelew@gmail.com>
2019-01-12 21:25:39 +0000

    Refactors task manager tests

--- test_task_manager.py
+++ test_task_manager.py
@@ -22,6 +22,9 @@ def generate_two_components(tm, identical = False):
     else:
         tm.allocate_resources(randint_comp_B)
 
+def first_worker(tm):
+    return tm.workers[0][0]
+
 @pytest.fixture(autouse=True)
 def new_tm():
     return Task_Manager()
@@ -34,22 +37,19 @@ def test_task_manager_allocates_workers_to_assembly_line(new_tm):
 
 def test_task_manager_allocates_component_A_to_worker_a1(new_tm):
     new_tm.allocate_resources(randint_comp_A)
-    first_worker = new_tm.workers[0][0]
-    assert 'A' in first_worker.components
+    assert 'A' in first_worker(new_tm).components
     assert 'A' not in new_tm.assembly_line.belt
 
 def test_task_manager_allocates_component_B_to_worker_a1(new_tm):
     generate_two_components(new_tm, identical = False)
-    first_worker = new_tm.workers[0][0]
-    assert 'A' in first_worker.components
-    assert 'B' in first_worker.components
-    assert not first_worker.collecting_mode
+    assert 'A' in first_worker(new_tm).components
+    assert 'B' in first_worker(new_tm).components
+    assert not first_worker(new_tm).collecting_mode
 
 def test_task_manager_does_not_allocate_identical_components_to_worker(new_tm):
     generate_two_components(new_tm, identical = True)
-    first_worker = new_tm.workers[0][0]
     second_worker = new_tm.workers[0][1]
-    assert first_worker.components.count('A') == 1
+    assert first_worker(new_tm).components.count('A') == 1
     assert second_worker.components.count('A') == 1
 
 def test_task_manager_instructs_worker_to_place_widget_if_slot_empty(new_tm):
