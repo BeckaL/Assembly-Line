e45d0e5079e284eb38918430420ad558ef9038da
Becka <rebecca.lelew@gmail.com>
2019-01-12 17:52:10 +0000

    Adds in manufacturing time for workers

--- test_worker.py
+++ test_worker.py
@@ -1,9 +1,34 @@
 from worker import Worker
 
+w = Worker()
+
 def test_worker_initializes_with_no_components():
-    w = Worker()
     assert w.components == []
 
 def test_worker_initializes_in_collecting_mode():
-    w = Worker()
     assert w.collecting_mode
+
+def test_worker_initializes_without_a_completed_widget():
+    assert w.widgets == None
+
+def test_worker_stores_new_component():
+    w.store_component('A')
+    assert w.components == ['A']
+
+def test_worker_with_two_components_not_in_collecting_mode():
+    w.store_component('B')
+    assert not w.collecting_mode
+
+def test_worker_does_not_accept_two_identical_components():
+    assert w.store_component('A') == False
+    assert len(w.components) == 2
+
+def test_worker_makes_widget_in_four_time_units():
+    w2 = Worker()
+    w2.store_component('A')
+    w2.store_component('B')
+    w2.time_pass()
+    assert not w2.collecting_mode
+    for i in range(4):
+        w2.time_pass()
+    assert w2.widgets == 'X'
--- worker.py
+++ worker.py
@@ -3,3 +3,20 @@ class Worker:
     def __init__(self):
         self.components = []
         self.collecting_mode = True
+        self.widgets = None
+        self.manufacturing_time = 4
+
+    def store_component(self, object):
+        if object not in self.components:
+            self.components.append(object)
+            if sorted(self.components) == ['A', 'B']:
+                self.collecting_mode = False
+            return True
+        else:
+            return False
+
+    def time_pass(self):
+        if self.manufacturing_time == 0:
+            self.widgets = 'X'
+        elif not self.collecting_mode:
+            self.manufacturing_time -= 1
